a7d7a8f08f5d691501f4274890396bb3fd0640dc
Artur Mądrzak <artur@madrzak.eu>
2016-03-18 10:42:29 +0100

    Headers, placeholders

---
+++ tests/modem/generic-test.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2016 Artur Mądrzak <artur@madrzak.eu>
+ * This program is free software. It comes without any warranty, to the extent
+ * permitted by applicable law. You can redistribute it and/or modify it under
+ * the terms of the Do What The Fuck You Want To Public License, Version 2, as
+ * published by Sam Hocevar. See the COPYING file for more details.
+ */
+
+#include <unity.h>
+
+
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_dummy(void)
+{
+    TEST_FAIL_MESSAGE("DUMMY TEST");
+}
+
--- tests/parser-test.c
+++ tests/parser-test.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2016 Artur Mądrzak <artur@madrzak.eu>
+ * This program is free software. It comes without any warranty, to the extent
+ * permitted by applicable law. You can redistribute it and/or modify it under
+ * the terms of the Do What The Fuck You Want To Public License, Version 2, as
+ * published by Sam Hocevar. See the COPYING file for more details.
+ */
+
 #include <unity.h>
 
 
@@ -14,3 +22,4 @@ void test_dummy(void)
 {
     TEST_FAIL_MESSAGE("DUMMY TEST");
 }
+
